be32115e53a34e60ece680376f81c72620abfd1f
Prokhorenko Vladyslav <random.person.1907@gmail.com>
2021-01-03 18:36:35 +0200

    Added vuejs, vue-router and vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,54 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import vuetify from "./plugins/vuetify";
+import VueRouter from 'vue-router';
+import TodoComponent from "./components/TodoComponent";
+
+window.Vue.use(VueRouter);
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+    { path: '/', component: TodoComponent },
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+    routes // short for `routes: routes`
+})
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+window.Vue.component('TodoComponent', TodoComponent);
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+    vuetify,
+    router
+});
